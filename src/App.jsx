b5d9b638359075326904a41f695c932631228e5e
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-21 15:17:39 +0700

    change siteProps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Raka Rmp",
+  title: "Front End Developer",
+  email: "rakaabdirmp@gmail.com",
+  gitHub: "grizz12",
+  instagram: "rakarmp_fja",
+  twitter: "RakaTrust",
 };
 
 const primaryColor = "#4E567E";
