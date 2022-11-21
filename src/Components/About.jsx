5097e85f94a8a126caf5e884a62d9b89c83e297e
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-21 15:18:22 +0700

    change skill detail

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,51 +1,16 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
-
 import React from "react";
-
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/mosaic.svg";
-
 const imageAltText = "purple and blue abstract background";
-
-/**
- * Sort description that expands on your title on the Home component.
- */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
+  "Saya adalah seorang pelajar yang sedang belajar di SMK Assalaam Bandung. Saya sangat tertarik dengan dunia IT dan saya ingin menjadi seorang Front End Developer.";
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "React JS",
+  "Vue JS",
+  "Next JS",
+  "AnyThing"
 ];
-
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Powerfull saat solo code dan powerfull saat team code";
 
 const About = () => {
   return (
